da638343260530751fc5bbf9aafe253a06e5506b
wagnerhumaita <107189708+wagnerhumaita@users.noreply.github.com>
2025-10-06 02:33:35 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,1 @@
-// Configuração do Firebase
-// IMPORTANTE: Substitua os valores abaixo pelas suas credenciais do Firebase
 
-const firebaseConfig = {
-    apiKey: "sua-api-key-aqui",
-    authDomain: "seu-projeto.firebaseapp.com",
-    projectId: "seu-projeto-id",
-    storageBucket: "seu-projeto.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "sua-app-id-aqui"
-};
-
-// Exportar configuração
-export default firebaseConfig;
